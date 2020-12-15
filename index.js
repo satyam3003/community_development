f72219b124d6f15ba3256d65181e474ec1c9c5f6
satyam3003 <75485302+satyam3003@users.noreply.github.com>
2020-12-15 23:08:20 +0530

    V1.4

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var ml4 = {};
-ml4.opacityIn = [0,1];
+ml4.opacityIn = [0, 1];
 ml4.scaleIn = [0.2, 1];
 ml4.scaleOut = 3;
 ml4.durationIn = 1600;
@@ -7,45 +7,52 @@ ml4.durationOut = 1200;
 ml4.delay = 1000;
 
 anime.timeline({loop: true})
-  .add({
-    targets: '.ml4 .letters-1',
-    opacity: ml4.opacityIn,
-    scale: ml4.scaleIn,
-    duration: ml4.durationIn
-  }).add({
+    .add({
+        targets: '.ml4 .letters-1',
+        opacity: ml4.opacityIn,
+        scale: ml4.scaleIn,
+        duration: ml4.durationIn
+    }).add({
     targets: '.ml4 .letters-1',
     opacity: 0,
     scale: ml4.scaleOut,
     duration: ml4.durationOut,
     easing: "easeInExpo",
     delay: ml4.delay
-  }).add({
+}).add({
     targets: '.ml4 .letters-2',
     opacity: ml4.opacityIn,
     scale: ml4.scaleIn,
     duration: ml4.durationIn
-  }).add({
+}).add({
     targets: '.ml4 .letters-2',
     opacity: 0,
     scale: ml4.scaleOut,
     duration: ml4.durationOut,
     easing: "easeInExpo",
     delay: ml4.delay
-  }).add({
+}).add({
     targets: '.ml4 .letters-3',
     opacity: ml4.opacityIn,
     scale: ml4.scaleIn,
     duration: ml4.durationIn
-  }).add({
+}).add({
     targets: '.ml4 .letters-3',
     opacity: 0,
     scale: ml4.scaleOut,
     duration: ml4.durationOut,
     easing: "easeInExpo",
     delay: ml4.delay
-  }).add({
+}).add({
     targets: '.ml4',
     opacity: 0,
     duration: 1000,
     delay: 1000
-  });
+});
+
+
+var preloader = document.getElementById('loading');
+
+function loader() {
+    preloader.style.display = 'none';
+}
